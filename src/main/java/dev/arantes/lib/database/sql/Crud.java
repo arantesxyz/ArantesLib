317caa33466150870015db3920e81fd99d9673b0
Tomás Duarte <tomasrsduarte@gmail.com>
2020-03-18 04:16:57 +0000

    Fix crud to prevent unclosed statements and result sets #15

--- src/main/java/dev/arantes/lib/database/sql/Crud.java
+++ src/main/java/dev/arantes/lib/database/sql/Crud.java
@@ -10,37 +10,69 @@ public class Crud {
         this.connection = connection;
         this.table = table;
     }
-    public void createTable(String format) throws SQLException {
-        PreparedStatement stm = connection.prepareStatement(
-                "CREATE TABLE IF NOT EXISTS " + table + " (" + format + ")"
-        );
-        stm.execute();
-        stm.close();
+
+    public void createTable(String format) {
+        PreparedStatement stm = null;
+
+        try {
+            stm = connection.prepareStatement(
+                    String.format("CREATE TABLE IF NOT EXISTS %s (%s)", table, format)
+            );
+
+            stm.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            finalizeStatement(stm);
+        }
     }
 
     public ResultSet executeQuery(String sql, Object... values) throws SQLException {
-        PreparedStatement stm = this.connection.prepareStatement(sql);
+        PreparedStatement stm = null;
+        ResultSet querySet = null;
 
-        for (int i = 0; i < values.length; i++) {
-            stm.setObject(i, values[i]);
+        try{
+            stm = connection.prepareStatement(sql);
+            for (int i = 0; i < values.length; i++) {
+                stm.setObject(i, values[i]);
+            }
+
+            querySet = stm.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            finalizeStatement(stm);
+
+            if (querySet != null) {
+                querySet.close();
+            }
         }
 
-        return stm.executeQuery();
+        return querySet;
     }
 
-    public void executeUpdate(String sql, Object... values) throws SQLException {
-        PreparedStatement stm = this.connection.prepareStatement(sql);
+    public void executeUpdate(String sql, Object... values) {
+        PreparedStatement stm = null;
 
-        for (int i = 0; i < values.length; i++) {
-            stm.setObject(i, values[i]);
+        try {
+            stm = connection.prepareStatement(sql);
+            for (int i = 0; i < values.length; i++) {
+                stm.setObject(i, values[i]);
+            }
+
+            stm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            finalizeStatement(stm);
         }
-
-        stm.executeUpdate();
     }
 
     public boolean update(String where, String query, Object... values) {
+        PreparedStatement stm = null;
+
         try {
-            PreparedStatement stm = connection.prepareStatement(
+            stm = connection.prepareStatement(
                     String.format("UPDATE %s SET %s WHERE %s", table, query, where)
             );
 
@@ -52,14 +84,18 @@ public class Crud {
             return true;
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            finalizeStatement(stm);
         }
 
         return false;
     }
 
     public boolean create(String query, Object... values) {
+        PreparedStatement stm = null;
+
         try {
-            PreparedStatement stm = connection.prepareStatement(
+            stm = connection.prepareStatement(
                     String.format("INSERT INTO %s VALUES (%s)", table, query)
             );
 
@@ -71,18 +107,40 @@ public class Crud {
             return true;
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            finalizeStatement(stm);
         }
 
         return false;
     }
 
     public ResultSet get(String field, String where) {
+        ResultSet resultSet = null;
+
         try {
-            return executeQuery(String.format("SELECT %s FROM %s WHERE %s", field, table, where));
+            resultSet = executeQuery(String.format("SELECT %s FROM %s WHERE %s", field, table, where));
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
-        return null;
+        return resultSet;
+    }
+
+    private void finalizeStatement(PreparedStatement stm) {
+        if (stm != null) {
+            try {
+                stm.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
